015bb412899d1731eb8cd9767eb01c3d7843bdb0
Michael Parrish <michael@zooniverse.org>
2014-11-19 14:15:33 +0000

    Extract validation matcher

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -4,14 +4,12 @@ RSpec.describe Comment, type: :model do
   context 'validating' do
     it 'should require a body' do
       without_body = build :comment, body: nil
-      expect(without_body).to_not be_valid
-      expect(without_body.errors[:body]).to include "can't be blank"
+      expect(without_body).to fail_validation body: "can't be blank"
     end
     
     it 'should require a user' do
       without_user = build :comment, user_id: nil
-      expect(without_user).to_not be_valid
-      expect(without_user.errors[:user]).to include "can't be blank"
+      expect(without_user).to fail_validation user: "can't be blank"
     end
   end
   
---
+++ spec/support/validation_matcher.rb
@@ -0,0 +1,30 @@
+RSpec::Matchers.define :fail_validation do |attrs_and_messages|
+  matching_errors = { }
+  
+  match do |record|
+    is_valid = record.valid?
+    return !is_valid unless attrs_and_messages.present?
+    
+    attrs_and_messages.each_pair do |attr, message|
+      errors = record.errors[attr] || []
+      matches = errors.select{ |m| m[message] }
+      matching_errors[attr] = matches.length > 0
+    end
+    
+    unmatched_error = matching_errors.values.include? false
+    !is_valid && !unmatched_error
+  end
+  
+  failure_message do |record|
+    message = ''
+    
+    matching_errors.each_pair do |attr, matches|
+      next if matches
+      message += "expected #{ record.class } to fail validation for #{ attr }"
+      message += " with #{ attrs_and_messages[attr] }" if message
+      message += "\nerrors were #{ actual.errors.messages[attr].inspect }\n"
+    end
+    
+    message
+  end
+end
