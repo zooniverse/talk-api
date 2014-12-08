546141af3f3a49966fa93b7dd7fb2d947addd991
Michael Parrish <michael@zooniverse.org>
2014-12-08 10:06:09 -0600

    Board policy

--- app/policies/board_policy.rb
+++ app/policies/board_policy.rb
@@ -1,4 +1,24 @@
 class BoardPolicy < ApplicationPolicy
+  def index?
+    true
+  end
+  
+  def show?
+    true
+  end
+  
+  def create?
+    moderator? || admin?
+  end
+  
+  def update?
+    moderator? || admin?
+  end
+  
+  def destroy?
+    moderator? || admin?
+  end
+  
   class Scope < Scope
     def resolve
       scope
--- spec/policies/board_policy_spec.rb
+++ spec/policies/board_policy_spec.rb
@@ -1,5 +1,28 @@
 require 'spec_helper'
 
 RSpec.describe BoardPolicy, type: :policy do
-  it 'should be spec\'d'
+  let(:user){ }
+  let(:record){ create :board, section: 'test' }
+  let(:subject){ BoardPolicy.new user, record }
+  
+  context 'without a user' do
+    it_behaves_like 'a policy permitting', :index, :show
+    it_behaves_like 'a policy forbidding', :create, :update, :destroy
+  end
+  
+  context 'with a user' do
+    let(:user){ create :user }
+    it_behaves_like 'a policy permitting', :index, :show
+    it_behaves_like 'a policy forbidding', :create, :update, :destroy
+  end
+  
+  context 'with a moderator' do
+    let(:user){ create :user, roles: { test: ['moderator'] } }
+    it_behaves_like 'a policy permitting', :index, :show, :create, :update, :destroy
+  end
+  
+  context 'with an admin' do
+    let(:user){ create :user, roles: { test: ['admin'] } }
+    it_behaves_like 'a policy permitting', :index, :show, :create, :update, :destroy
+  end
 end
