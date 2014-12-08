75ec849c3a6bec28d0bb8bf2ee91e97024821d6b
Michael Parrish <michael@zooniverse.org>
2014-12-08 16:24:37 -0600

    Tag policy

--- app/policies/tag_policy.rb
+++ app/policies/tag_policy.rb
@@ -1,4 +1,24 @@
 class TagPolicy < ApplicationPolicy
+  def index?
+    true
+  end
+  
+  def show?
+    true
+  end
+  
+  def create?
+    false
+  end
+  
+  def update?
+    false
+  end
+  
+  def destroy?
+    false
+  end
+  
   class Scope < Scope
     def resolve
       scope
--- spec/policies/tag_policy_spec.rb
+++ spec/policies/tag_policy_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 RSpec.describe TagPolicy, type: :policy do
-  it 'should be spec\'d'
+  let(:user){ }
+  let(:record){ create :tag }
+  let(:subject){ TagPolicy.new user, record }
+  
+  context 'without a user' do
+    it_behaves_like 'a policy permitting', :index, :show
+    it_behaves_like 'a policy forbidding', :create, :update, :destroy
+  end
 end
