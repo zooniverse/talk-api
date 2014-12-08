89d03f4a2e99fbea77ba830901bacec1e48697ca
Michael Parrish <michael@zooniverse.org>
2014-12-08 16:28:23 -0600

    Moderation policy

--- app/policies/moderation_policy.rb
+++ app/policies/moderation_policy.rb
@@ -1,4 +1,24 @@
 class ModerationPolicy < ApplicationPolicy
+  def index?
+    moderator? || admin?
+  end
+  
+  def show?
+    moderator? || admin?
+  end
+  
+  def create?
+    logged_in?
+  end
+  
+  def update?
+    moderator? || admin?
+  end
+  
+  def destroy?
+    moderator? || admin?
+  end
+  
   class Scope < Scope
     def resolve
       scope
--- spec/policies/moderation_policy_spec.rb
+++ spec/policies/moderation_policy_spec.rb
@@ -1,5 +1,27 @@
 require 'spec_helper'
 
 RSpec.describe ModerationPolicy, type: :policy do
-  it 'should be spec\'d'
+  let(:user){ }
+  let(:record){ create :moderation, section: 'test' }
+  let(:subject){ ModerationPolicy.new user, record }
+  
+  context 'without a user' do
+    it_behaves_like 'a policy forbidding', :index, :show, :create, :update, :destroy
+  end
+  
+  context 'with a user' do
+    let(:user){ create :user }
+    it_behaves_like 'a policy permitting', :create
+    it_behaves_like 'a policy forbidding', :index, :show, :update, :destroy
+  end
+  
+  context 'with a moderator' do
+    let(:user){ create :user, roles: { test: ['moderator'] } }
+    it_behaves_like 'a policy permitting', :index, :show, :create, :update, :destroy
+  end
+  
+  context 'with an admin' do
+    let(:user){ create :user, roles: { test: ['admin'] } }
+    it_behaves_like 'a policy permitting', :index, :show, :create, :update, :destroy
+  end
 end
