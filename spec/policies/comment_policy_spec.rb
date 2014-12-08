50d5188d61a1ed9c6870eadb6e4a0298da29b48f
Michael Parrish <michael@zooniverse.org>
2014-12-08 10:27:12 -0600

    Comment policy

--- spec/policies/comment_policy_spec.rb
+++ spec/policies/comment_policy_spec.rb
@@ -1,5 +1,35 @@
 require 'spec_helper'
 
 RSpec.describe CommentPolicy, type: :policy do
-  it 'should be spec\'d'
+  let(:user){ }
+  let(:record){ create :comment }
+  let(:subject){ CommentPolicy.new user, record }
+  
+  context 'without a user' do
+    it_behaves_like 'a policy permitting', :index, :show
+    it_behaves_like 'a policy forbidding', :create, :update, :destroy
+  end
+  
+  context 'with a user' do
+    let(:user){ create :user }
+    it_behaves_like 'a policy permitting', :index, :show, :create
+    it_behaves_like 'a policy forbidding', :update, :destroy
+  end
+  
+  context 'with the owner' do
+    let(:user){ record.user }
+    it_behaves_like 'a policy permitting', :index, :show, :create, :update, :destroy
+  end
+  
+  context 'with a moderator' do
+    let(:user){ create :user, roles: { test: ['moderator'] } }
+    it_behaves_like 'a policy permitting', :index, :show, :create
+    it_behaves_like 'a policy forbidding', :update, :destroy
+  end
+  
+  context 'with an admin' do
+    let(:user){ create :user, roles: { test: ['admin'] } }
+    it_behaves_like 'a policy permitting', :index, :show, :create
+    it_behaves_like 'a policy forbidding', :update, :destroy
+  end
 end
