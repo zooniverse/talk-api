1fc92f4ee8a16e9e45aea1a54e4612254bdbe021
Michael Parrish <michael@zooniverse.org>
2014-12-08 10:41:14 -0600

    User policy

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,4 +1,24 @@
 class UserPolicy < ApplicationPolicy
+  def index?
+    logged_in?
+  end
+  
+  def show?
+    true
+  end
+  
+  def create?
+    false
+  end
+  
+  def update?
+    logged_in? && user.id == record.id
+  end
+  
+  def destroy?
+    false
+  end
+  
   class Scope < Scope
     def resolve
       scope
--- spec/policies/user_policy_spec.rb
+++ spec/policies/user_policy_spec.rb
@@ -1,5 +1,36 @@
 require 'spec_helper'
 
 RSpec.describe UserPolicy, type: :policy do
-  it 'should be spec\'d'
+  let(:user){ }
+  let(:record){ create :user }
+  let(:subject){ UserPolicy.new user, record }
+  
+  context 'without a user' do
+    it_behaves_like 'a policy permitting', :show
+    it_behaves_like 'a policy forbidding', :index, :create, :update, :destroy
+  end
+  
+  context 'with a user' do
+    let(:user){ create :user }
+    it_behaves_like 'a policy permitting', :index, :show
+    it_behaves_like 'a policy forbidding', :create, :update, :destroy
+  end
+  
+  context 'with the logged in user' do
+    let(:user){ record }
+    it_behaves_like 'a policy permitting', :index, :show, :update
+    it_behaves_like 'a policy forbidding', :create, :destroy
+  end
+  
+  context 'with a moderator' do
+    let(:user){ create :user, roles: { test: ['moderator'] } }
+    it_behaves_like 'a policy permitting', :index, :show
+    it_behaves_like 'a policy forbidding', :create, :update, :destroy
+  end
+  
+  context 'with an admin' do
+    let(:user){ create :user, roles: { test: ['admin'] } }
+    it_behaves_like 'a policy permitting', :index, :show
+    it_behaves_like 'a policy forbidding', :create, :update, :destroy
+  end
 end
