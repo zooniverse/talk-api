cf8994788639bb6471404a1dfbd240ce547e3afc
Michael Parrish <michael@zooniverse.org>
2014-12-08 10:19:50 -0600

    Collection policy

--- app/policies/collection_policy.rb
+++ app/policies/collection_policy.rb
@@ -1,4 +1,24 @@
 class CollectionPolicy < ApplicationPolicy
+  def index?
+    true
+  end
+  
+  def show?
+    true
+  end
+  
+  def create?
+    logged_in?
+  end
+  
+  def update?
+    owner?
+  end
+  
+  def destroy?
+    owner?
+  end
+  
   class Scope < Scope
     def resolve
       scope
