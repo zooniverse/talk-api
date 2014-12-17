c9e11a240abedf512b17ee34d063142ef04af757
Michael Parrish <michael@zooniverse.org>
2014-12-17 11:44:30 -0600

    Board schema

--- app/schemas/board_schema.rb
+++ app/schemas/board_schema.rb
@@ -1,11 +1,21 @@
 class BoardSchema
   include JSON::SchemaBuilder
+  configure{ |opts| opts.strict = true }
+  
+  root :boards
   
   def create
-    
+    root do
+      string :title, required: true
+      string :description, required: true
+      string :section
+    end
   end
   
   def update
-    
+    root do
+      string :title, required: true
+      string :description, required: true
+    end
   end
 end
--- spec/schemas/board_schema_spec.rb
+++ spec/schemas/board_schema_spec.rb
@@ -1,5 +1,46 @@
 require 'spec_helper'
 
 RSpec.describe BoardSchema, type: :schema do
-  it 'should be spec\'d'
+  describe '#create' do
+    it_behaves_like 'a schema', :create do
+      let(:expected_json) do
+        {
+          type: :object,
+          required: [:boards],
+          properties: {
+            boards: {
+              type: :object,
+              required: [:title, :description],
+              properties: {
+                title: { type: :string },
+                description: { type: :string },
+                section: { type: :string }
+              }
+            }
+          }
+        }
+      end
+    end
+  end
+  
+  describe '#update' do
+    it_behaves_like 'a schema', :update do
+      let(:expected_json) do
+        {
+          type: :object,
+          required: [:boards],
+          properties: {
+            boards: {
+              type: :object,
+              required: [:title, :description],
+              properties: {
+                title: { type: :string },
+                description: { type: :string }
+              }
+            }
+          }
+        }
+      end
+    end
+  end
 end
