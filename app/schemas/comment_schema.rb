d44c36d322e2b973285cfb494430127c2d28ac89
Michael Parrish <michael@zooniverse.org>
2014-12-17 12:52:29 -0600

    Comment schema

--- app/schemas/comment_schema.rb
+++ app/schemas/comment_schema.rb
@@ -1,11 +1,24 @@
 class CommentSchema
   include JSON::SchemaBuilder
+  attr_accessor :policy
+  
+  root :comments
   
   def create
-    
+    root do
+      string  :category
+      string  :body,          required: true
+      integer :focus_id
+      integer :discussion_id, required: true
+    end
   end
   
   def update
-    
+    root do
+      string  :category
+      string  :body,          required: true
+      integer :discussion_id, required: true if policy.move?
+      integer :focus_id
+    end
   end
 end
--- spec/schemas/comment_schema_spec.rb
+++ spec/schemas/comment_schema_spec.rb
@@ -1,5 +1,79 @@
 require 'spec_helper'
 
 RSpec.describe CommentSchema, type: :schema do
-  it 'should be spec\'d'
+  describe '#create' do
+    it_behaves_like 'a schema', :create do
+      let(:expected_json) do
+        {
+          type: :object,
+          required: [:comments],
+          properties: {
+            comments: {
+              type: :object,
+              required: [:body, :discussion_id],
+              properties: {
+                category: { type: :string },
+                body: { type: :string },
+                focus_id: { type: :integer },
+                discussion_id: { type: :integer }
+              }
+            }
+          }
+        }
+      end
+    end
+  end
+  
+  describe '#update' do
+    context 'when moveable' do
+      it_behaves_like 'a schema', :update do
+        let(:record){ create :comment }
+        let(:user){ record.user }
+        let(:schema_context){ { policy: CommentPolicy.new(user, record) } }
+        let(:expected_json) do
+          {
+            type: :object,
+            required: [:comments],
+            properties: {
+              comments: {
+                type: :object,
+                required: [:body, :discussion_id],
+                properties: {
+                  category: { type: :string },
+                  body: { type: :string },
+                  focus_id: { type: :integer },
+                  discussion_id: { type: :integer }
+                }
+              }
+            }
+          }
+        end
+      end
+    end
+    
+    context 'when not moveable' do
+      it_behaves_like 'a schema', :update do
+        let(:record){ create :comment }
+        let(:user){ create :user }
+        let(:schema_context){ { policy: CommentPolicy.new(user, record) } }
+        let(:expected_json) do
+          {
+            type: :object,
+            required: [:comments],
+            properties: {
+              comments: {
+                type: :object,
+                required: [:body],
+                properties: {
+                  category: { type: :string },
+                  body: { type: :string },
+                  focus_id: { type: :integer }
+                }
+              }
+            }
+          }
+        end
+      end
+    end
+  end
 end
