f1b9d48400f8ad06b31900fb170bfc26507b2c0a
Michael Parrish <michael@zooniverse.org>
2015-02-12 09:54:24 -0600

    Remove unnecessary Restpack monkeypatch

--- config/initializers/restpack_serializer.rb
+++ config/initializers/restpack_serializer.rb
@@ -1,17 +1,3 @@
-require 'restpack_serializer'
-
-module RestPack::Serializer
-  class SideLoadDataBuilder
-    # Bug fix.  Prevent side loaded association from try to find non-existant records
-    def side_load_belongs_to
-      foreign_keys = @models.map { |model| model.send(@association.foreign_key) }.uniq.compact
-      side_load = foreign_keys.any? ? @association.klass.find(foreign_keys) : []
-      json_model_data = side_load.map { |model| @serializer.as_json(model) }
-      { @association.plural_name.to_sym => json_model_data, meta: { } }
-    end
-  end
-end
-
 # preload autoloaded serializers
 Dir[Rails.root.join('app/serializers/**/*.rb')].sort.each do |path|
   name = path.match(/serializers\/(.+)\.rb$/)[1]
