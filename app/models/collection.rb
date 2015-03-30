c9bc8cd379d4ca92582fda142af618e5c339f772
Michael Parrish <michael@zooniverse.org>
2015-03-30 16:50:24 -0500

    Searchable collections

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,6 +1,10 @@
 class Collection < ActiveRecord::Base
   include Focusable
+  include Searchable::Querying
   belongs_to :project
+  
+  class_attribute :searchable_klass
+  self.searchable_klass = SearchableCollection
   
   moderatable_with :destroy, by: [:moderator, :admin]
   moderatable_with :ignore, by: [:moderator, :admin]
