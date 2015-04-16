b23b08038f5b4a32642577bb915099e274fa254a
Michael Parrish <michael@zooniverse.org>
2015-04-16 15:11:39 -0500

    Include board permissions in serializer

--- app/serializers/board_serializer.rb
+++ app/serializers/board_serializer.rb
@@ -1,6 +1,6 @@
 class BoardSerializer
   include TalkSerializer
-  all_attributes except: :permissions
+  all_attributes
   can_include :discussions, :parent, :sub_boards
   can_sort_by :created_at
   self.default_sort = 'created_at'
