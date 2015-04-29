edd83b185121c5d6c93e52c717135e4fa9ca44c4
Michael Parrish <michael@zooniverse.org>
2015-04-29 11:34:09 -0500

    Notification policy

---
+++ app/policies/notification_policy.rb
@@ -0,0 +1,27 @@
+class NotificationPolicy < ApplicationPolicy
+  def index?
+    logged_in?
+  end
+  
+  def show?
+    owner?
+  end
+  
+  def create?
+    false
+  end
+  
+  def update?
+    owner?
+  end
+  
+  def destroy?
+    false
+  end
+  
+  class Scope < Scope
+    def resolve
+      scope.where user_id: user.id
+    end
+  end
+end
