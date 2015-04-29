e5ad83fa60ae8f9d54e1631971d4e5564344ac15
Michael Parrish <michael@zooniverse.org>
2015-04-29 12:47:24 -0500

    Announcement service

---
+++ app/services/announcement_service.rb
@@ -0,0 +1,2 @@
+class AnnouncementService < ApplicationService
+end
---
+++ spec/services/announcement_service_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+RSpec.describe AnnouncementService, type: :service do
+  it_behaves_like 'a service', Announcement do
+    let(:current_user){ create :admin, section: 'test' }
+    let(:parent_board){ create :announcement }
+    let(:create_params) do
+      {
+        announcements: {
+          message: 'works',
+          section: 'test',
+          expires_at: 1.hour.from_now.as_json
+        }
+      }
+    end
+    
+    it_behaves_like 'a service creating', Announcement
+    it_behaves_like 'a service updating', Announcement do
+      let(:current_user){ create :admin, section: 'zooniverse' }
+      let(:update_params) do
+        {
+          id: record.id,
+          announcements: {
+            message: 'works better',
+            section: 'zooniverse',
+            expires_at: 2.hours.from_now.as_json
+          }
+        }
+      end
+    end
+  end
+end
