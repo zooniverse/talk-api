522020476dc56d4a0d4435b10974b1b37b7894b7
Michael Parrish <michael@zooniverse.org>
2015-06-02 17:11:38 -0500

    Subscription policy

---
+++ app/policies/subscription_policy.rb
@@ -0,0 +1,23 @@
+class SubscriptionPolicy < ApplicationPolicy
+  def index?
+    logged_in?
+  end
+  
+  def show?
+    owner?
+  end
+  
+  def create?
+    false
+  end
+  
+  def update?
+    owner?
+  end
+  
+  class Scope < Scope
+    def resolve
+      scope.where user: user
+    end
+  end
+end
