be82e2e8431b464815f16e30dabc1823cee1edce
Michael Parrish <michael@zooniverse.org>
2015-06-02 17:09:23 -0500

    Ensure default preferences are created on access

--- app/controllers/subscription_preferences_controller.rb
+++ app/controllers/subscription_preferences_controller.rb
@@ -1,3 +1,9 @@
 class SubscriptionPreferencesController < ApplicationController
   include TalkResource
+  
+  before_action :ensure_defaults
+  
+  def ensure_defaults
+    SubscriptionPreference.for_user(current_user) if current_user
+  end
 end
