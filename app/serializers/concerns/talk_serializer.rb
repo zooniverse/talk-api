d4de67ae9b9f59cfeb6985a75ea10799f04a6f9b
Michael Parrish <michael@zooniverse.org>
2015-06-03 13:58:55 -0500

    Bump restpack fork

--- app/serializers/concerns/talk_serializer.rb
+++ app/serializers/concerns/talk_serializer.rb
@@ -12,28 +12,6 @@ module TalkSerializer
     
     is_sectioned = model_class.columns_hash.has_key?('section') rescue false
     can_filter_by(:section) if is_sectioned
-    
-    def klass.resource(params = {}, scope = nil, context = {})
-      return super if params.is_a?(model_class)
-      params.select{ |key, val| key =~ /^sort_linked/ }.each_pair do |type, values|
-        begin
-          type = type.match(/^sort_linked_(.*)$/)[1]
-          linked_serializer = "#{ type.singularize }_serializer".classify.safe_constantize
-          sortable = linked_serializer.serializable_sorting_attributes
-          ordering = values.split(',').collect do |value|
-            direction = value =~ /^\-/ ? 'desc' : 'asc'
-            value = value.gsub(/^\-/, '')
-            next unless sortable.include?(value.to_sym)
-            "#{ value } #{ direction }"
-          end.compact
-          
-          context[:sort_links] ||= { }
-          context[:sort_links][type] = ordering.join(', ') if ordering.any?
-        rescue
-        end
-      end
-      super
-    end
   end
   
   module ClassMethods
