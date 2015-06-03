63683f584921f78aa16c572ca0c4b650935c7997
Michael Parrish <michael@zooniverse.org>
2015-06-03 09:40:51 -0500

    Allow serialized notifications to include their subscriptions

--- app/serializers/notification_serializer.rb
+++ app/serializers/notification_serializer.rb
@@ -3,5 +3,6 @@ class NotificationSerializer
   all_attributes
   can_sort_by :created_at
   can_filter_by :section, :delivered
+  can_include :subscription
   self.default_sort = 'created_at'
 end
--- spec/serializers/notification_serializer_spec.rb
+++ spec/serializers/notification_serializer_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
 
 RSpec.describe NotificationSerializer, type: :serializer do
-  it_behaves_like 'a talk serializer', exposing: :all
+  it_behaves_like 'a talk serializer', exposing: :all, including: [:subscription]
 end
