9d279a5f457d35b7854076e5ccd7d29c9a38f7ee
Michael Parrish <michael@zooniverse.org>
2015-06-04 15:00:50 -0500

    Ensure board permissions are checked for all comment actions.  Towards #37

--- app/policies/comment_policy.rb
+++ app/policies/comment_policy.rb
@@ -1,41 +1,67 @@
 class CommentPolicy < ApplicationPolicy
-  delegate :locked?, to: 'record.discussion'
+  delegate :writable?, to: :discussion_policy
   
   def index?
     true
   end
   
   def show?
-    true
+    discussion_policy.show?
   end
   
   def create?
-    logged_in? && !locked?
+    logged_in? && !locked? && writable?
   end
   
   def update?
-    owner? && !locked?
+    owner? && !locked? && writable?
   end
   
   def destroy?
-    owner? && !locked?
+    owner? && !locked? && writable?
   end
   
   def move?
-    !locked? && (owner? || moderator? || admin?)
+    !locked? && (owner? || moderator? || admin?) && writable?
   end
   
   def upvote?
-    logged_in? && !owner?
+    logged_in? && !owner? && writable?
   end
   
   def remove_upvote?
-    logged_in? && !owner?
+    logged_in? && !owner? && writable?
+  end
+  
+  def locked?
+    Array.wrap(record).any? do |comment|
+      comment.discussion.locked?
+    end
+  end
+  
+  def discussion_policy
+    DiscussionPolicy.new user, discussions
+  end
+  
+  def discussions
+    Array.wrap(record).compact.collect &:discussion
   end
   
   class Scope < Scope
+    delegate :zooniverse_admin?, :permissions, to: :@discussion_scope
+    
+    def initialize(user, scope)
+      @discussion_scope = DiscussionPolicy::Scope.new user, Discussion
+      super
+    end
+    
     def resolve
-      scope
+      return scope.all if zooniverse_admin?
+      scope.joins(discussion: :board).where permissions.join(' or ')
+    end
+    
+    def discussion_scope
+      DiscussionPolicy::Scope.new user, Discussion
     end
   end
 end
--- spec/policies/comment_policy_spec.rb
+++ spec/policies/comment_policy_spec.rb
@@ -2,36 +2,153 @@ require 'spec_helper'
 
 RSpec.describe CommentPolicy, type: :policy do
   let(:user){ }
-  let(:record){ create :comment, section: 'test' }
+  let(:board){ create :board }
+  let(:discussion){ create :discussion, board: board }
+  let(:record){ create :comment, discussion: discussion }
   let(:subject){ CommentPolicy.new user, record }
   
-  context 'without a user' do
-    it_behaves_like 'a policy permitting', :index, :show
-    it_behaves_like 'a policy forbidding', :create, :update, :destroy, :move, :upvote, :remove_upvote
+  context 'with permissions read:all write:all' do
+    let(:board){ create :board, section: 'test', permissions: { read: 'all', write: 'all' } }
+    
+    context 'without a user' do
+      it_behaves_like 'a policy permitting', :index, :show
+      it_behaves_like 'a policy forbidding', :create, :update, :destroy, :move, :upvote, :remove_upvote
+    end
+    
+    context 'with a user' do
+      let(:user){ create :user }
+      it_behaves_like 'a policy permitting', :index, :show, :create, :upvote, :remove_upvote
+      it_behaves_like 'a policy forbidding', :update, :destroy, :move
+    end
+    
+    context 'with the owner' do
+      let(:user){ record.user }
+      it_behaves_like 'a policy permitting', :index, :show, :create, :update, :destroy, :move
+      it_behaves_like 'a policy forbidding', :upvote, :remove_upvote
+    end
+    
+    context 'with a moderator' do
+      let(:user){ create :moderator }
+      it_behaves_like 'a policy permitting', :index, :show, :create, :move, :upvote, :remove_upvote
+      it_behaves_like 'a policy forbidding', :update, :destroy
+    end
+    
+    context 'with an admin' do
+      let(:user){ create :admin }
+      it_behaves_like 'a policy permitting', :index, :show, :create, :move, :upvote, :remove_upvote
+      it_behaves_like 'a policy forbidding', :update, :destroy
+    end
   end
   
-  context 'with a user' do
-    let(:user){ create :user }
-    it_behaves_like 'a policy permitting', :index, :show, :create, :upvote, :remove_upvote
-    it_behaves_like 'a policy forbidding', :update, :destroy, :move
+  context 'with permissions read:team write:team' do
+    let(:board){ create :board, section: 'test', permissions: { read: 'team', write: 'team' } }
+    
+    context 'without a user' do
+      it_behaves_like 'a policy excluding'
+      it_behaves_like 'a policy forbidding', :show, :create, :update, :destroy, :move, :upvote, :remove_upvote
+    end
+    
+    context 'with a user' do
+      let(:user){ create :user }
+      it_behaves_like 'a policy excluding'
+      it_behaves_like 'a policy forbidding', :show, :create, :update, :destroy, :move, :upvote, :remove_upvote
+    end
+    
+    context 'with a team member' do
+      let(:user){ create :scientist }
+      it_behaves_like 'a policy permitting', :index, :show, :create
+      it_behaves_like 'a policy forbidding', :update, :destroy
+    end
+    
+    context 'with a moderator' do
+      let(:user){ create :moderator }
+      it_behaves_like 'a policy permitting', :index, :show, :create, :move, :upvote, :remove_upvote
+      it_behaves_like 'a policy forbidding', :update, :destroy
+    end
+    
+    context 'with an admin' do
+      let(:user){ create :admin }
+      it_behaves_like 'a policy permitting', :index, :show, :create, :move, :upvote, :remove_upvote
+      it_behaves_like 'a policy forbidding', :update, :destroy
+    end
   end
   
-  context 'with the owner' do
-    let(:user){ record.user }
-    it_behaves_like 'a policy permitting', :index, :show, :create, :update, :destroy, :move
-    it_behaves_like 'a policy forbidding', :upvote, :remove_upvote
+  context 'with permissions read:moderator write:moderator' do
+    let(:board){ create :board, section: 'test', permissions: { read: 'moderator', write: 'moderator' } }
+    
+    context 'without a user' do
+      it_behaves_like 'a policy excluding'
+      it_behaves_like 'a policy forbidding', :show, :create, :update, :destroy, :move, :upvote, :remove_upvote
+    end
+    
+    context 'with a user' do
+      let(:user){ create :user }
+      it_behaves_like 'a policy excluding'
+      it_behaves_like 'a policy forbidding', :show, :create, :update, :destroy, :move, :upvote, :remove_upvote
+    end
+    
+    context 'with a team member' do
+      let(:user){ create :scientist }
+      it_behaves_like 'a policy excluding'
+      it_behaves_like 'a policy forbidding', :show, :create, :update, :destroy, :move, :upvote, :remove_upvote
+    end
+    
+    context 'with the owner' do
+      let(:user){ record.user }
+      it_behaves_like 'a policy excluding'
+      it_behaves_like 'a policy forbidding', :show, :create, :update, :destroy, :move, :upvote, :remove_upvote
+    end
+    
+    context 'with a moderator' do
+      let(:user){ create :moderator }
+      it_behaves_like 'a policy permitting', :index, :show, :create, :move, :upvote, :remove_upvote
+      it_behaves_like 'a policy forbidding', :update, :destroy
+    end
+    
+    context 'with an admin' do
+      let(:user){ create :admin }
+      it_behaves_like 'a policy permitting', :index, :show, :create, :move, :upvote, :remove_upvote
+      it_behaves_like 'a policy forbidding', :update, :destroy
+    end
   end
   
-  context 'with a moderator' do
-    let(:user){ create :moderator }
-    it_behaves_like 'a policy permitting', :index, :show, :create, :move, :upvote, :remove_upvote
-    it_behaves_like 'a policy forbidding', :update, :destroy
-  end
-  
-  context 'with an admin' do
-    let(:user){ create :admin }
-    it_behaves_like 'a policy permitting', :index, :show, :create, :move, :upvote, :remove_upvote
-    it_behaves_like 'a policy forbidding', :update, :destroy
+  context 'with permissions read:admin write:admin' do
+    let(:board){ create :board, section: 'test', permissions: { read: 'admin', write: 'admin' } }
+    
+    context 'without a user' do
+      it_behaves_like 'a policy excluding'
+      it_behaves_like 'a policy forbidding', :show, :create, :update, :destroy, :move, :upvote, :remove_upvote
+    end
+    
+    context 'with a user' do
+      let(:user){ create :user }
+      it_behaves_like 'a policy excluding'
+      it_behaves_like 'a policy forbidding', :show, :create, :update, :destroy, :move, :upvote, :remove_upvote
+    end
+    
+    context 'with a team member' do
+      let(:user){ create :scientist }
+      it_behaves_like 'a policy excluding'
+      it_behaves_like 'a policy forbidding', :show, :create, :update, :destroy, :move, :upvote, :remove_upvote
+    end
+    
+    context 'with the owner' do
+      let(:user){ record.user }
+      it_behaves_like 'a policy excluding'
+      it_behaves_like 'a policy forbidding', :show, :create, :update, :destroy, :move, :upvote, :remove_upvote
+    end
+    
+    context 'with a moderator' do
+      let(:user){ create :moderator }
+      it_behaves_like 'a policy excluding'
+      it_behaves_like 'a policy forbidding', :show, :create, :update, :destroy, :move, :upvote, :remove_upvote
+    end
+    
+    context 'with an admin' do
+      let(:user){ create :admin }
+      it_behaves_like 'a policy permitting', :index, :show, :create, :move, :upvote, :remove_upvote
+      it_behaves_like 'a policy forbidding', :update, :destroy
+    end
   end
   
   context 'with a locked discussion' do
@@ -45,5 +162,69 @@ RSpec.describe CommentPolicy, type: :policy do
       it_behaves_like 'a policy permitting', :index, :show
       it_behaves_like 'a policy forbidding', :create, :update, :destroy, :move, :upvote, :remove_upvote
     end
+  end
+  
+  context 'with scope' do
+    let(:public_board){ create :board, section: '1-project' }
+    let(:public_discussion){ create :discussion, board: public_board }
+    let!(:public_comments){ create_list :comment, 2, discussion: public_discussion }
+    
+    let(:team_board){ create :board, section: '1-project', permissions: { read: 'team', write: 'team' } }
+    let(:team_discussion){ create :discussion, board: team_board }
+    let(:team_comments){ create_list :comment, 2, discussion: team_discussion }
+    
+    let(:admin_board){ create :board, section: '1-project', permissions: { read: 'admin', write: 'admin' } }
+    let(:admin_discussion){ create :discussion, board: admin_board }
+    let(:admin_comments){ create_list :comment, 2, discussion: admin_discussion }
+    
+    let(:moderator_board){ create :board, section: '1-project', permissions: { read: 'moderator', write: 'moderator' } }
+    let(:moderator_discussion){ create :discussion, board: moderator_board }
+    let(:moderator_comments){ create_list :comment, 2, discussion: moderator_discussion }
+    
+    let(:other_board){ create :board, section: '2-project', permissions: { read: 'admin', write: 'admin' } }
+    let(:other_discussion){ create :discussion, board: other_board }
+    let(:other_comments){ create_list :comment, 2, discussion: other_discussion }
+    
+    let(:subject){ CommentPolicy::Scope.new(user, Comment).resolve }
+    
+    context 'without a user' do
+      it{ is_expected.to match_array public_comments }
+    end
+    
+    context 'with a scientist' do
+      let(:user){ create :scientist, section: '1-project' }
+      it{ is_expected.to match_array public_comments + team_comments }
+    end
+    
+    context 'with a moderator' do
+      let(:user){ create :moderator, section: '1-project' }
+      it{ is_expected.to match_array public_comments + team_comments + moderator_comments }
+    end
+    
+    context 'with an admin' do
+      let(:user){ create :admin, section: '1-project' }
+      it{ is_expected.to match_array public_comments + team_comments + moderator_comments + admin_comments }
+    end
+    
+    context 'with a zooniverse admin' do
+      let(:user){ create :admin, section: 'zooniverse' }
+      it{ is_expected.to match_array public_comments + team_comments + moderator_comments + admin_comments + other_comments }
+    end
+  end
+  
+  context 'with mixed permissions' do
+    let(:user){ create :moderator, section: '1-project' }
+    
+    let(:permitted_board){ create :board, section: '1-project', permissions: { read: 'moderator', write: 'moderator' } }
+    let(:permitted_discussion){ create :discussion, board: permitted_board }
+    let(:permitted_comment){ create :comment, discussion: permitted_discussion }
+    
+    let(:unpermitted_board){ create :board, section: '2-project', permissions: { read: 'moderator', write: 'moderator' } }
+    let(:unpermitted_discussion){ create :discussion, board: unpermitted_board }
+    let(:unpermitted_comment){ create :comment, discussion: unpermitted_discussion }
+    
+    let(:record){ [permitted_comment, unpermitted_comment] }
+    
+    it_behaves_like 'a policy forbidding', :show, :create, :update, :destroy
   end
 end
