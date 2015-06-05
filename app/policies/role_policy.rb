9f3e6f84fad484d19acfceca4d99f84d88b03d3a
Michael Parrish <michael@zooniverse.org>
2015-06-05 09:46:28 -0500

    Make roles public for index and show

--- app/policies/role_policy.rb
+++ app/policies/role_policy.rb
@@ -1,10 +1,10 @@
 class RolePolicy < ApplicationPolicy
   def index?
-    admin?
+    true
   end
   
   def show?
-    accessible_section?
+    true
   end
   
   def create?
@@ -19,32 +19,22 @@ class RolePolicy < ApplicationPolicy
     accessible_section?
   end
   
-  concerning :PrivilegedSections do
-    def privileged_sections
-      return [] unless user
-      @privileged_sections ||= user.roles.where(name: 'admin').distinct(:section).pluck :section
+  def privileged_sections
+    return [] unless user
+    @privileged_sections ||= user.roles.where(name: 'admin').distinct(:section).pluck :section
+  end
+  
+  def accessible_section?
+    return true if privileged_sections.include?('zooniverse')
+    Array.wrap(record).each do |r|
+      return false unless privileged_sections.include?(r.section)
     end
-    
-    def accessible_section?
-      return true if privileged_sections.include?('zooniverse')
-      Array.wrap(record).each do |r|
-        return false unless privileged_sections.include?(r.section)
-      end
-      true
-    end
+    true
   end
   
   class Scope < Scope
-    include PrivilegedSections
-    
     def resolve
-      if !user
-        scope.none
-      elsif user.roles.where(section: 'zooniverse', name: 'admin').exists?
-        scope.all
-      else
-        scope.where 'section = any(array[:sections])', sections: privileged_sections
-      end
+      scope
     end
   end
 end
