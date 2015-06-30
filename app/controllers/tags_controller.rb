b77e33984eded44852c842f8942bc9d152a80c03
Michael Parrish <michael@zooniverse.org>
2015-06-30 14:54:31 -0500

    Fake json-api format for popular tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -7,12 +7,36 @@ class TagsController < ApplicationController
     limit = clamp params.fetch(:limit, 10).to_i, (1..20)
     scoped = Tag.in_section params[:section]
     scoped = scoped.of_type(params[:type]) if params[:type]
+    scoped = scoped.popular limit: limit
     render json: {
-      tags: scoped.popular(limit: limit).keys
+      tags: tags_from(scoped.keys),
+      meta: popular_meta_for(scoped.count, limit)
     }
   end
   
   protected
+  
+  def popular_meta_for(count, limit)
+    {
+      tags: {
+        page: 1,
+        page_size: limit,
+        count: count,
+        include: [],
+        page_count: 1,
+        previous_page: nil,
+        next_page: nil,
+        first_href: '/tags/popular',
+        previous_href: nil,
+        next_href: nil,
+        last_href: '/tags/popular'
+      }
+    }
+  end
+  
+  def tags_from(names)
+    names.collect{ |name| Tag.new(name: name, section: params[:section], taggable_type: params[:type]) }
+  end
   
   def clamp(value, range)
     value = [value, range.min].max
--- spec/controllers/tags_controller_spec.rb
+++ spec/controllers/tags_controller_spec.rb
@@ -27,7 +27,24 @@ RSpec.describe TagsController, type: :controller do
     RSpec.shared_context 'TagsController#popular' do
       let(:params){ { } }
       subject{ response }
+      let(:tags){ response.json['tags'] }
+      let(:tag_names){ tags.collect{ |h| h['name'] } }
       before(:each){ get :popular, params }
+    end
+    
+    RSpec.shared_examples_for 'TagsController#popular_meta_for' do |expected_count: nil, expected_limit: nil|
+      subject{ OpenStruct.new response.json['meta']['tags'] }
+      its(:page){ is_expected.to eql 1 }
+      its(:page_size){ is_expected.to eql expected_limit }
+      its(:count){ is_expected.to eql expected_count }
+      its(:include){ is_expected.to eql [] }
+      its(:page_count){ is_expected.to eql 1 }
+      its(:previous_page){ is_expected.to be_nil }
+      its(:next_page){ is_expected.to be_nil }
+      its(:first_href){ is_expected.to eql '/tags/popular' }
+      its(:previous_href){ is_expected.to be_nil }
+      its(:next_href){ is_expected.to be_nil }
+      its(:last_href){ is_expected.to eql '/tags/popular' }
     end
     
     context 'without a section' do
@@ -38,30 +55,50 @@ RSpec.describe TagsController, type: :controller do
     
     context 'with an empty section' do
       include_context 'TagsController#popular'
+      it_behaves_like 'TagsController#popular_meta_for', expected_count: 0, expected_limit: 10
       let(:params){ { section: 'project-1' } }
       it{ is_expected.to be_successful }
-      its(:json){ is_expected.to eql 'tags' => [] }
+      
+      describe 'response' do
+        subject{ tag_names }
+        it{ is_expected.to eql [] }
+      end
     end
     
     context 'with a section' do
       include_context 'TagsController#popular'
+      it_behaves_like 'TagsController#popular_meta_for', expected_count: 4, expected_limit: 10
       let(:params){ { section: section } }
       it{ is_expected.to be_successful }
-      its(:json){ is_expected.to eql 'tags' => %w(subject_most collection_most subject_least collection_least) }
+      
+      describe 'response' do
+        subject{ tag_names }
+        it{ is_expected.to eql %w(subject_most collection_most subject_least collection_least) }
+      end
     end
     
     context 'with a type' do
       include_context 'TagsController#popular'
+      it_behaves_like 'TagsController#popular_meta_for', expected_count: 2, expected_limit: 10
       let(:params){ { section: section, type: 'subject' } }
       it{ is_expected.to be_successful }
-      its(:json){ is_expected.to eql 'tags' => %w(subject_most subject_least) }
+      
+      describe 'response' do
+        subject{ tag_names }
+        it{ is_expected.to eql %w(subject_most subject_least) }
+      end
     end
     
     context 'with a limit' do
       include_context 'TagsController#popular'
+      it_behaves_like 'TagsController#popular_meta_for', expected_count: 1, expected_limit: 1
       let(:params){ { section: section, type: 'subject', limit: 1 } }
       it{ is_expected.to be_successful }
-      its(:json){ is_expected.to eql 'tags' => %w(subject_most) }
+      
+      describe 'response' do
+        subject{ tag_names }
+        it{ is_expected.to eql %w(subject_most) }
+      end
     end
   end
 end
