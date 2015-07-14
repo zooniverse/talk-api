3c2f96cb809aa93daec496c96028c89f77631f1e
Michael Parrish <michael@zooniverse.org>
2015-07-14 11:44:29 -0500

    Organize controller rescuing and add RestPack::Serializer::InvalidInclude and ActiveRecord::RecordNotUnique

--- app/controllers/concerns/action_rescuing.rb
+++ app/controllers/concerns/action_rescuing.rb
@@ -4,19 +4,24 @@ module ActionRescuing
   included do
     rescue_from ActiveRecord::RecordNotFound, with: :not_found
     rescue_from ActiveRecord::RecordInvalid, with: :bad_request
+    rescue_from ActiveRecord::RecordNotUnique, with: :conflict
     rescue_from ActiveRecord::UnknownAttributeError, with: :unprocessable
+    rescue_from ActiveRecord::RecordNotDestroyed, with: :unauthorized
+    
     rescue_from ActionController::UnpermittedParameters, with: :unprocessable
-    rescue_from TalkService::ParameterError, with: :unprocessable
     rescue_from ActionController::RoutingError, with: :not_found
+    rescue_from ActionController::ParameterMissing, with: :unprocessable
+    
+    rescue_from JSON::Schema::ValidationError, with: :unprocessable
     
     rescue_from Pundit::AuthorizationNotPerformedError, with: :not_implemented
     rescue_from Pundit::PolicyScopingNotPerformedError, with: :not_implemented
     rescue_from Pundit::NotDefinedError, with: :not_implemented
     rescue_from Pundit::NotAuthorizedError, with: :unauthorized
-    rescue_from ActiveRecord::RecordNotDestroyed, with: :unauthorized
     
-    rescue_from JSON::Schema::ValidationError, with: :unprocessable
-    rescue_from ActionController::ParameterMissing, with: :unprocessable
+    rescue_from RestPack::Serializer::InvalidInclude, with: :bad_request
+    
+    rescue_from TalkService::ParameterError, with: :unprocessable
     rescue_from Talk::BannedUserError, with: :forbidden
   end
   
@@ -42,6 +47,10 @@ module ActionRescuing
   
   def not_implemented(exception)
     render_exception :not_implemented, exception
+  end
+  
+  def conflict(exception)
+    render_exception :conflict, exception
   end
   
   def render_exception(status, exception)
--- spec/support/shared_examples_for_controller_rescuing.rb
+++ spec/support/shared_examples_for_controller_rescuing.rb
@@ -22,19 +22,35 @@ end
 
 RSpec.shared_examples_for 'a controller rescuing' do
   it_behaves_like 'ActionRescuing',
+    ActiveRecord::RecordNotFound.new, with: 404
+  
+  it_behaves_like 'ActionRescuing',
     ActiveRecord::RecordInvalid.new(Board.new.tap(&:valid?)), with: 400
   
   it_behaves_like 'ActionRescuing',
-    ActiveRecord::RecordNotFound.new, with: 404
+    ActiveRecord::RecordNotUnique.new('foo'), with: 409
+  
+  it_behaves_like 'ActionRescuing',
+    ActiveRecord::UnknownAttributeError.new('foo', 'bar'), with: 422
+  
+  it_behaves_like 'ActionRescuing',
+    ActiveRecord::RecordNotDestroyed.new('foo'), with: 401
   
   it_behaves_like 'ActionRescuing',
     ActionController::UnpermittedParameters.new(['foo']), with: 422
   
   it_behaves_like 'ActionRescuing',
-    TalkService::ParameterError.new, with: 422
+    ActionController::RoutingError.new('foo'), with: 404
   
   it_behaves_like 'ActionRescuing',
-    ActionController::RoutingError.new('foo'), with: 404
+    ActionController::ParameterMissing.new('foo'), with: 422
+  
+  # Provide a faked JSON::Schema::ValidationError
+  json_schema_validation_error = JSON::Schema::ValidationError.new 'foo', [], 'bar', OpenStruct.new(uri: '')
+  def json_schema_validation_error.to_s; message; end
+  
+  it_behaves_like 'ActionRescuing',
+    json_schema_validation_error, with: 422
   
   it_behaves_like 'ActionRescuing',
     Pundit::AuthorizationNotPerformedError.new, with: 501
@@ -49,13 +65,10 @@ RSpec.shared_examples_for 'a controller rescuing' do
     Pundit::NotAuthorizedError.new, with: 401
   
   it_behaves_like 'ActionRescuing',
-    ActiveRecord::RecordNotDestroyed.new('foo'), with: 401
+    RestPack::Serializer::InvalidInclude.new('foo'), with: 400
   
   it_behaves_like 'ActionRescuing',
-    ActionController::ParameterMissing.new('foo'), with: 422
-  
-  it_behaves_like 'ActionRescuing',
-    ActiveRecord::UnknownAttributeError.new('foo', 'bar'), with: 422
+    TalkService::ParameterError.new, with: 422
   
   it_behaves_like 'ActionRescuing',
     Talk::BannedUserError.new, with: 403
