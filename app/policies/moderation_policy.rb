3f13e9d4d04ee13f32f2c073f7dcb9d6c3c6d36b
Michael Parrish <michael@zooniverse.org>
2015-07-14 16:02:48 -0500

    Ensure moderation target is found before applying available actions

--- app/policies/moderation_policy.rb
+++ app/policies/moderation_policy.rb
@@ -25,12 +25,16 @@ class ModerationPolicy < ApplicationPolicy
   
   def available_actions
     return [] unless logged_in?
-    @available_actions ||= record.target.class.moderatable.select do |action, roles|
+    @available_actions ||= target.class.moderatable.select do |action, roles|
       roles[:all] ||
       (roles[:moderator] && moderator?) ||
       (roles[:admin] && admin?) ||
       (roles[:team] && team?)
     end.keys
+  end
+  
+  def target
+    record.target || (raise ActiveRecord::RecordNotFound.new("Couldn't find target"))
   end
   
   class Scope < Scope
