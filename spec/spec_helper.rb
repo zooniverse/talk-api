d296f8de99893acfee085818864994e74357c934
Michael Parrish <michael@zooniverse.org>
2015-07-31 10:45:23 -0500

    Restore webmock

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,7 @@ require 'spec_helper'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
 require 'json/schema_builder/rspec'
+require 'webmock/rspec'
 require 'sidekiq/testing'
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 ActiveRecord::Migration.maintain_test_schema!
@@ -20,6 +21,9 @@ Aws.config.update region: 'us-east-1', credentials: Aws::Credentials.new('', '')
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
   config.include JSON::SchemaBuilder::RSpecHelper, type: :schema
+  
+  config.before(:suite){ WebMock.disable_net_connect! }
+  config.after(:suite){ WebMock.allow_net_connect! }
   
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
