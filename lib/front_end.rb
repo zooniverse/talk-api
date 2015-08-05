081e3e86b3df89f1161f9347fbc220a318b0c4bd
Michael Parrish <michael@zooniverse.org>
2015-08-05 11:25:42 -0500

    Build front-end links.  Closes #50

--- lib/front_end.rb
+++ lib/front_end.rb
@@ -10,4 +10,33 @@ class FrontEnd
   def self.zooniverse_talk
     "#{ host }/#/talk"
   end
+  
+  def self.project_talk(project)
+    "#{ host }/#/projects/#{ project.slug }/talk"
+  end
+  
+  def self.talk_root_for(object)
+    if object.try(:project)
+      project_talk object.project
+    else
+      zooniverse_talk
+    end
+  end
+  
+  def self.link_to(object)
+    case object
+    when Board
+      "#{ talk_root_for object }/#{ object.id }"
+    when Comment
+      "#{ talk_root_for object }/#{ object.discussion.board_id }/#{ object.discussion_id }?comment=#{ object.id }"
+    when Discussion
+      "#{ talk_root_for object }/#{ object.board_id }/#{ object.id }"
+    when Project
+      "#{ host }/#/projects/#{ object.slug }"
+    when User
+      "#{ host }/#/users/#{ object.login }"
+    when UserConversation
+      "#{ host }/#/inbox/#{ object.conversation_id }"
+    end
+  end
 end
--- spec/lib/front_end_spec.rb
+++ spec/lib/front_end_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe FrontEnd, type: :lib do
       its(:host){ is_expected.to eql production_host }
     end
     
-    context "in other environemtns" do
+    context 'in other environments' do
       let(:is_production){ false }
       its(:host){ is_expected.to eql other_host }
     end
@@ -22,5 +22,75 @@ RSpec.describe FrontEnd, type: :lib do
   describe '.zooniverse_talk' do
     before(:each){ expect(subject).to receive(:host).and_return 'host' }
     its(:zooniverse_talk){ is_expected.to eql 'host/#/talk' }
+  end
+  
+  describe '.project_talk' do
+    let(:project){ create :project, slug: 'foo/bar' }
+    before(:each){ expect(subject).to receive(:host).and_return 'host' }
+    
+    it 'should link to the project talk' do
+      expect(subject.project_talk(project)).to eql 'host/#/projects/foo/bar/talk'
+    end
+  end
+  
+  describe '.talk_root_for' do
+    let(:project){ create :project, slug: 'foo/bar' }
+    before(:each){ expect(subject).to receive(:host).and_return 'host' }
+    
+    context 'with a project' do
+      let(:board){ create :board, section: "project-#{ project.id }" }
+      
+      it 'should link to the project talk' do
+        expect(subject.talk_root_for(board)).to eql 'host/#/projects/foo/bar/talk'
+      end
+    end
+    
+    context 'without a project' do
+      let(:board){ create :board, section: 'zooniverse' }
+      
+      it 'should link to the zooniverse talk' do
+        expect(subject.talk_root_for(board)).to eql 'host/#/talk'
+      end
+    end
+  end
+  
+  describe '.link_to' do
+    RSpec.shared_examples_for 'FrontEnd.link_to' do |klass, expected|
+      context "with a #{ klass }" do
+        before(:each){ expect(subject).to receive(:host).and_return 'host' }
+        
+        it "should link to the #{ klass } path" do
+          expect(FrontEnd.link_to(object)).to eql expected
+        end
+      end
+    end
+    
+    it_behaves_like 'FrontEnd.link_to', Board, 'host/#/talk/123' do
+      let(:object){ create :board, id: 123 }
+    end
+    
+    it_behaves_like 'FrontEnd.link_to', Comment, 'host/#/talk/1/2?comment=3' do
+      let(:board){ create :board, id: 1 }
+      let(:discussion){ create :discussion, id: 2, board: board }
+      let(:object){ create :comment, id: 3, discussion: discussion }
+    end
+    
+    it_behaves_like 'FrontEnd.link_to', Discussion, 'host/#/talk/1/2' do
+      let(:board){ create :board, id: 1 }
+      let(:object){ create :discussion, id: 2, board: board }
+    end
+    
+    it_behaves_like 'FrontEnd.link_to', Project, 'host/#/projects/foo/bar' do
+      let(:object){ create :project, slug: 'foo/bar' }
+    end
+    
+    it_behaves_like 'FrontEnd.link_to', User, 'host/#/users/Foo' do
+      let(:object){ create :user, login: 'Foo' }
+    end
+    
+    it_behaves_like 'FrontEnd.link_to', UserConversation, 'host/#/inbox/123' do
+      let(:conversation){ create :conversation_with_messages, id: 123 }
+      let(:object){ create :user_conversation, conversation: conversation }
+    end
   end
 end
