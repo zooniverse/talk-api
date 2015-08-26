aaeb4a5fec5d6c85dc740731cd15775b10edbf89
Michael Parrish <michael@zooniverse.org>
2015-08-26 16:08:49 -0500

    Add BlockedUserPolicy

---
+++ spec/policies/blocked_user_policy_spec.rb
@@ -0,0 +1,50 @@
+require 'spec_helper'
+
+RSpec.describe BlockedUserPolicy, type: :policy do
+  let(:user){ }
+  let(:record){ create :blocked_user }
+  let(:subject){ BlockedUserPolicy.new user, record }
+  
+  context 'without a user' do
+    it_behaves_like 'a policy forbidding', :index, :show, :create, :update, :destroy
+  end
+  
+  context 'without an authorized user' do
+    let(:user){ create :user }
+    it_behaves_like 'a policy permitting', :index, :create
+    it_behaves_like 'a policy forbidding', :show, :update, :destroy
+  end
+  
+  context 'with the owner' do
+    let(:user){ record.user }
+    it_behaves_like 'a policy permitting', :index, :show, :create, :destroy
+    it_behaves_like 'a policy forbidding', :update
+  end
+  
+  context 'with a project admin' do
+    let(:user){ create :admin, section: 'something-else' }
+    it_behaves_like 'a policy permitting', :index, :create
+    it_behaves_like 'a policy forbidding', :show, :update, :destroy
+  end
+  
+  context 'with a moderator' do
+    let(:user){ create :moderator, section: 'zooniverse' }
+    it_behaves_like 'a policy permitting', :index, :show, :create
+    it_behaves_like 'a policy forbidding', :update, :destroy
+  end
+  
+  context 'with an admin' do
+    let(:user){ create :admin, section: 'zooniverse' }
+    it_behaves_like 'a policy permitting', :index, :show, :create
+    it_behaves_like 'a policy forbidding', :update, :destroy
+  end
+  
+  context 'with scope' do
+    let!(:other_records){ create_list :blocked_user, 2 }
+    let(:user){ create :user }
+    let(:records){ create_list :blocked_user, 2, user: user }
+    let(:subject){ BlockedUserPolicy::Scope.new(user, BlockedUser).resolve }
+    
+    it{ is_expected.to match_array records }
+  end
+end
