7b2047e4832faefa27167ff3bd0b5686716d4bfa
Michael Parrish <michael@zooniverse.org>
2015-09-11 10:45:51 -0500

    Explicitly require honeybadger

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,7 @@ require 'active_record/railtie'
 require 'action_controller/railtie'
 require 'action_mailer/railtie'
 require 'rails/test_unit/railtie'
+require 'honeybadger'
 
 Bundler.require(*Rails.groups)
 
