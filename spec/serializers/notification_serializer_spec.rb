d169a8845f41698e925621f6a2a163be29230762
Michael Parrish <michael@zooniverse.org>
2015-09-25 14:47:14 -0500

    Include notification source in serializer response

--- spec/serializers/notification_serializer_spec.rb
+++ spec/serializers/notification_serializer_spec.rb
@@ -3,5 +3,11 @@ require 'spec_helper'
 RSpec.describe NotificationSerializer, type: :serializer do
   it_behaves_like 'a talk serializer', exposing: :all, including: [:subscription] do
     it_behaves_like 'a serializer with embedded attributes', relations: [:project]
+    
+    context 'with a source' do
+      subject{ json }
+      it{ is_expected.to include :source }
+      its([:source]){ is_expected.to include :id => model_instance.source.id.to_s }
+    end
   end
 end
