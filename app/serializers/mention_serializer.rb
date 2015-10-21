86ed4c40e19e9f3048e7a95b74080b6aff652efd
Michael Parrish <michael@zooniverse.org>
2015-10-21 15:37:25 -0500

    Update MentionSerializer

--- app/serializers/mention_serializer.rb
+++ app/serializers/mention_serializer.rb
@@ -1,5 +1,9 @@
 class MentionSerializer
   include TalkSerializer
   all_attributes
-  can_include :comment, :user
+  
+  can_include :comment
+  can_filter_by :section
+  can_sort_by :created_at
+  self.default_sort = '-created_at'
 end
