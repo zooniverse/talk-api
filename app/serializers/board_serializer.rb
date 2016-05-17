6a8c129b1551b6b6280457fdd2915eb51d481f06
Michael Parrish <michael@zooniverse.org>
2016-05-17 15:41:32 -0500

    Allow BoardSerializer to sort by id

--- app/serializers/board_serializer.rb
+++ app/serializers/board_serializer.rb
@@ -5,7 +5,7 @@ class BoardSerializer
   all_attributes
   attribute :latest_discussion
   can_filter_by :subject_default
-  can_sort_by :position, :last_comment_created_at
+  can_sort_by :id, :position, :last_comment_created_at
   embed_attributes_from :project
   self.default_sort = 'position,-last_comment_created_at'
   self.preloads = [:latest_discussion]
