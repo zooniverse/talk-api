009a3c4aca7a4143d49972ee953c2089fc0e8183
Campbell Allen <campbell.allen@gmail.com>
2017-02-22 12:10:47 +0000

    better configs setup during container start (#106)
    * don't attempt to symlink configs in migrate.sh
    
    allow the start.sh to setup the config files via docker entrypoint
    
    * always attempt symlink and ignore failures

--- docker/start.sh
+++ docker/start.sh
@@ -1,8 +1,5 @@
 #!/bin/bash -ex
 
-if [ -z "$VAGRANT_APP" ]
-then
-    ln -s /rails_conf/* /rails_app/config/
-fi
+ln -s /rails_conf/* /rails_app/config/ || true
 
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/talk.conf
