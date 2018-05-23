b561f285c7b46e41e58fd2ed1d77dff31e225f94
Marten Veldthuis <marten@veldthuis.com>
2018-05-23 12:16:58 +0100

    Remove searchable_collections

--- db/migrate/20180518132406_drop_collections.rb
+++ db/migrate/20180518132406_drop_collections.rb
@@ -1,6 +1,18 @@
 class DropCollections < ActiveRecord::Migration
   def change
     ActiveRecord::Base.connection.execute <<-SQL
+      do language plpgsql $$
+        begin
+          create or replace view searches as
+            select * from searchable_boards
+            union all
+            select * from searchable_comments
+            union all
+            select * from searchable_discussions;
+        end;
+      $$;
+      DROP MATERIALIZED VIEW IF EXISTS searchable_collections;
+
       DROP FOREIGN TABLE IF EXISTS collections;
       DROP FOREIGN TABLE IF EXISTS collection_subjects;
       DROP SEQUENCE IF EXISTS collections_id_seq;
