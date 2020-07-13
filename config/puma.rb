d6a0484c85c136cb0beb5e2baf382cf49a1cc41b
Zach Wolfenbarger <zach.wolfenbarger@gmail.com>
2020-07-13 16:47:52 -0500

    Puma threads aren't env dependent

--- config/puma.rb
+++ config/puma.rb
@@ -4,12 +4,7 @@ if ENV['RAILS_ENV'] !~ /development|test/
   directory '/rails_app'
 end
 
-if ENV['RAILS_ENV'] == 'staging'
-  threads 2, 5
-else
-  threads 1, ENV.fetch('RAILS_MAX_THREADS', 2).to_i
-end
-
+threads 1, ENV.fetch('RAILS_MAX_THREADS', 2).to_i
 worker_timeout 10
 
 bind 'tcp://0.0.0.0:81'
