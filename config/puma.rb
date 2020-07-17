64a3006840698b6a1256c2e9e6bbe345fe7bb0cf
Campbell Allen <campbell.allen@gmail.com>
2020-07-17 15:45:24 +0100

    update the puma config
    remove the worker_timeout
    add tag for known process
    allow ports to be different for the dev / deployed envs
    track pid / state in local tmp files

--- config/puma.rb
+++ config/puma.rb
@@ -1,10 +1,15 @@
-#!/usr/bin/env puma
+# frozen_string_literal: true
 
-if ENV['RAILS_ENV'] !~ /development|test/
-  directory '/rails_app'
-end
+app_path = File.expand_path(File.dirname(File.dirname(__FILE__)))
+dev_env = 'development'
+rails_env = ENV['RAILS_ENV'] || dev_env
+port = rails_env == dev_env ? 3000 : 81
+threads_count = ENV.fetch('RAILS_MAX_THREADS', 2).to_i
 
-threads 1, ENV.fetch('RAILS_MAX_THREADS', 2).to_i
-worker_timeout 10
+# PUMA DSL settings
+pidfile "#{app_path}/tmp/pids/server.pid"
+state_path "#{app_path}/tmp/pids/puma.state"
+threads 1, threads_count
+bind "tcp://0.0.0.0:#{port}"
+tag 'talk_api'
 
-bind 'tcp://0.0.0.0:81'
