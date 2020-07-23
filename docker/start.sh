aeb273f6443839ebc319eba9a73e13e7bc95edd5
Campbell Allen <campbell.allen@gmail.com>
2020-07-23 14:31:52 +0100

    Serve static assets via nginx reverse proxy (#229)
    * add commit file directly to public dir
    
    * remove unsused startup code
    
    * use the 9.5 postgres images
    
    match the db version we're running
    
    * remove unused comments
    
    * update readme on using docker
    
    * serve static assets from the nginx reverse proxy
    
    * remove changes to sidekiq

--- docker/start.sh
+++ docker/start.sh
@@ -2,13 +2,6 @@
 
 tmpreaper 7d /tmp/
 
-ln -s /rails_conf/* /rails_app/config/ || true
-
-if [ -f "commit_id.txt" ]
-then
-  cp commit_id.txt public/
-fi
-
 cd /rails_app
 mkdir -p tmp/pids
 rm -f tmp/pids/*.pid
