1efa8267ee9985d35ca03823ad9ac2bcde4ed84b
yuenmichelle1 <michelle.dano.yuen@gmail.com>
2024-05-28 11:34:57 -0500

    ActionController::TestResponse injerits from ActionDispatch::TestResponse in Rails 4. Rails 5 test responses are instances of ActionDispatch::TestResponse

--- spec/support/api_helpers.rb
+++ spec/support/api_helpers.rb
@@ -1,12 +1,3 @@
-# TODO: Can Remove Lines 2-6 (definition of ActionController::TestResponse#json) once on Rails 5
-class ActionController::TestResponse
-  def json
-    @_json ||= JSON.parse(body).with_indifferent_access
-  end
-end
-
-# On Rails 5, Test responses do not inherit from ctionController::TestResponse
-
 class ActionDispatch::TestResponse
   def json
     @_json ||= JSON.parse(body).with_indifferent_access
