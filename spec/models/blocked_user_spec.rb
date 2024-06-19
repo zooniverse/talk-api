8168ca61eb41810169e66dca64372b0a93db4330
tooyosi <oluwatoyosi.oyegoke@gmail.com>
2024-06-19 19:29:17 +0100

    remove validation text

--- spec/models/blocked_user_spec.rb
+++ spec/models/blocked_user_spec.rb
@@ -9,12 +9,12 @@ RSpec.describe BlockedUser, type: :model do
   context 'validating' do
     it 'should require a user' do
       without_user = build :blocked_user, user: nil
-      expect(without_user).to fail_validation user: $required_validation_text
+      expect(without_user).to fail_validation
     end
 
     it 'should require a blocked_user' do
       without_blocked_user = build :blocked_user, blocked_user: nil
-      expect(without_blocked_user).to fail_validation blocked_user: $required_validation_text
+      expect(without_blocked_user).to fail_validation
     end
   end
 
