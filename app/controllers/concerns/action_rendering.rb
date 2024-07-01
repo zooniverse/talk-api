f815faedee7ba58c14a44c48dda73cc33b9806d0
yuenmichelle1 <michelle@zooniverse.org>
2024-07-01 17:49:56 -0500

    Rails 5 - Comments Controller Spec fix (#336)
    * permit params and convert to_hash because params no longer inherits from HashWithIndifferentAccess
    
    * fix backwards breaking behavior. update moderations_controller. need to permit params in order for added attributes to be included when converting to a hash with to_h
    
    * permit params on index and then convert params .to_h so serializer.page has access to parameters

--- app/controllers/concerns/action_rendering.rb
+++ app/controllers/concerns/action_rendering.rb
@@ -5,7 +5,8 @@ module ActionRendering
     authorize model_class
     scoped = policy_scope model_class
     params[:sort] ||= serializer_class.default_sort if serializer_class.default_sort
-    render json: serializer_class.page(params, scoped, current_user: current_user)
+    params.permit!
+    render json: serializer_class.page(params.to_h, scoped, current_user: current_user)
   end
 
   def show
