3797fac9258ea4dc0291ec6dce0362beffc8bb7e
yuenmichelle1 <michelle@zooniverse.org>
2024-08-08 12:47:26 -0500

    Rails 5 deprecation warnings on config attributes (#352)
    * Deprecation warning - serve_static_files is deprecated. Replace with public_file_server.enabled
    
    * update static_cache_control with replaced public_file_server.headers and remove unneccessary raise_transactional_callbacks

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,14 +5,13 @@ Rails.application.configure do
   config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
   config.action_controller.allow_forgery_protection = false
-  config.serve_static_files = false
+  config.public_file_server.enabled = false
   config.force_ssl = true
   config.log_level = :info
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
   config.log_formatter = ::Logger::Formatter.new
   config.active_record.dump_schema_after_migration = false
-  config.active_record.raise_in_transactional_callbacks = true
 
   config.action_mailer.smtp_settings = {
     enable_starttls_auto: true,
