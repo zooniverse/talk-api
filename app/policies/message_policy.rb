0fe76a217a5341bab2215425193d1bf8c54b1e76
yuenmichelle1 <michelle@zooniverse.org>
2025-01-28 11:37:22 -0600

    Rails 6.1 - specify reference to alias table on query within message_policy (#404)
    * update message_policy .where on join query to reference table alias name
    
    https://blog.saeloun.com/2021/01/25/rails-6-allow-where-clause-reference-association-by-alias-name/
    
    * Add version check on alias table query due to backwards incompatible change from 6.0 to 6.1

--- app/policies/message_policy.rb
+++ app/policies/message_policy.rb
@@ -34,9 +34,21 @@ class MessagePolicy < ApplicationPolicy
 
   class Scope < Scope
     def resolve
-      scope.joins(:conversation, :user_conversations).where({
+      # TODO: REMOVE VERSION CHECK ONCE ON RAILS 6.1
+      # Rails 6.1 introduces a feature that allows .where
+      # to access alias tables.
+      # See: https://blog.saeloun.com/2021/01/25/rails-6-allow-where-clause-reference-association-by-alias-name/
+      # This change is not backwards compatible with Rails 6.0
+
+      if Rails.version.starts_with?('6.0')
+        scope.joins(:conversation, :user_conversations).where({
         user_conversations: { user_id: user.id }
-      })
+        })
+      else
+        scope.joins(:conversation, :user_conversations).where({
+        user_conversations_messages: { user_id: user.id }
+        })
+      end
     end
   end
 end
