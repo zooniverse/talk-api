dc13842fcd9081000a770775ad4143176aea032e
yuenmichelle1 <michelle@zooniverse.org>
2025-03-13 10:08:26 -0500

    dual boot rails 7.0. (#421)
    - Update Gemfile.next.lock and Gemfile to use rails 7.0
    - Use configs_for in db_config with db.rake and rake tasks. Deprecation warning in db_config : DEPRECATION WARNING: [] is deprecated and will be removed from Rails 7.0 (Use configs_for) (called from db_config at (pry):2)
    - Update autoloading with autoload on boot and on each reload.  See:  https://guides.rubyonrails.org/autoloading_and_reloading_constants.html#autoloading-when-the-application-boots

--- config/initializers/restpack_serializer.rb
+++ config/initializers/restpack_serializer.rb
@@ -1,9 +1,13 @@
 # preload autoloaded serializers after database has been loaded
-db_loaded = ::ActiveRecord::Base.connection_pool.with_connection(&:active?) rescue false
+# See: https://guides.rubyonrails.org/autoloading_and_reloading_constants.html#autoloading-when-the-application-boots
 
-if db_loaded
-  Dir[Rails.root.join('app/serializers/**/*.rb')].sort.each do |path|
-    name = path.match(/serializers\/(.+)\.rb$/)[1]
-    name.classify.constantize unless path =~ /serializers\/concerns/
+Rails.application.config.to_prepare do
+  db_loaded = ::ActiveRecord::Base.connection_pool.with_connection(&:active?) rescue false
+
+  if db_loaded
+    Dir[Rails.root.join('app/serializers/**/*.rb')].sort.each do |path|
+      name = path.match(/serializers\/(.+)\.rb$/)[1]
+      name.classify.constantize unless path =~ /serializers\/concerns/
+    end
   end
 end
