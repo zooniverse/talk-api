453c7c6e5ab1a3afd2bd29bdb71aac0aa45e26b2
yuenmichelle1 <michelle@zooniverse.org>
2025-04-02 15:59:51 -0500

    update activerecord base configurations reference on panoptes_user.r to configs_for to return hash (#426)
    See: https://www.bigbinary.com/blog/rails-6-changed-activerecord-base-configurations-result-to-an-object

--- app/models/panoptes_user.rb
+++ app/models/panoptes_user.rb
@@ -1,4 +1,4 @@
 class PanoptesUser < ApplicationRecord
   self.table_name = 'users'
-  establish_connection configurations["panoptes_#{ Rails.env }"]
+  establish_connection configurations.configs_for(env_name: "panoptes_#{Rails.env}")[0].configuration_hash
 end
