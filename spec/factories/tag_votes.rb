6cf426deb30016b56388bbe46e211979a122c56c
yuenmichelle1 <michelle.dano.yuen@gmail.com>
2025-06-30 22:56:00 -0500

    rubocop fixes of specs

--- spec/factories/tag_votes.rb
+++ spec/factories/tag_votes.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :tag_vote do
     user
--- spec/factories/votable_tag.rb
+++ spec/factories/votable_tag.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :votable_tag do
-    name { "Vote For This Tag" }
+    name { 'Vote For This Tag' }
     project_id { 1 }
-    section { "project-12" }
+    section { 'project-12' }
     taggable { nil }
   end
 end
